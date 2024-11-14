a94a4bca05de52db01f796c7f14c7db347ae0820
Jasper Bruin <62376435+jasperbruin@users.noreply.github.com>
2024-11-14 16:24:40 +0100

    Delete src/harberger/toggle_server.sh

--- src/harberger/toggle_server.sh
+++
@@ -1,9 +0,0 @@
-curl -X POST "http://localhost:10341/spawn-agents" \
--H "Content-Type: application/json" \
--d '{
-    "username": "jasper",
-    "password": "dteMpzfEnS7B8fX3nph3smy54bZffRVS",
-    "players": 3,
-    "hostname": "188.166.34.67",
-    "game_params": "game.json"
-}'
